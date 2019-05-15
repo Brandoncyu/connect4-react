e9e79282a6c5c870a7f77ef8a13cec5dfeec726c
Brandon <brandoncyu@gmail.com>
2019-05-14 18:06:39 -0700

    set game over setting

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import {
   consecutiveRightDown, 
   consecutiveLeftUp 
 } from './components/counting-algorithims/count'
+import {Container, Row} from 'reactstrap'
 
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-      board: [ [1], [1, 1], [2, 1], [1, 2, 2, 1], [], [], [] ],
+      board: [ [], [], [], [], [], [], [] ],
       player: 1,
       gameOver: false,
       lastRow: 0,
@@ -36,38 +37,35 @@ class App extends Component {
     })
   }
 
-  setTurn = (number) =>{
-    this.addToColumn(number)
-    
-    if(this.state.columnFull === false){
-      this.checkBoard()
-      
-    }
-  }
 
-  addToColumn = (number) => {
-    let column = this.state.board[number]
-    if (column.length < 6){
+  addToColumn = (columnNumber) => {
+    let column = this.state.board[columnNumber]
+    if (column.length < 6 && !this.state.gameOver){
       column = [...column, this.state.player]
       const lastRow = column.length - 1
-      const board = [...this.state.board.slice(0, number), column, ...this.state.board.slice(number + 1)]
+      const board = [...this.state.board.slice(0, columnNumber), column, ...this.state.board.slice(columnNumber + 1)]
       
-      this.setState({ board, lastRow, lastColumn: number, columnFull: false })
-    } else{
-      this.setState({columnFull: true})
-    }
-    
+      let gameOver = this.checkBoard(board, lastRow, columnNumber, this.state.player)
+      console.log(gameOver)
+      if (gameOver){
+        this.setState({ board, gameOver })
+      } else{
+        this.setState({board})
+        this.toggleUser()
+      }
+    } 
   }
 
-  checkBoard = () => {
-    const { board, lastRow, lastColumn, player} = this.state
+  checkBoard = (board, lastRow, lastColumn, player) => {
     const verticalCheck = consecutiveDown(board, lastRow, lastColumn, player)
     const horizontalCheck = consecutiveLeft(board, lastRow, lastColumn, player) + consecutiveRight(board, lastRow, lastColumn, player)
     const backslashCheck = consecutiveLeftDown(board, lastRow, lastColumn, player) + consecutiveRightUp(board, lastRow, lastColumn, player)
     const forwardSlashCheck = consecutiveRightDown(board, lastRow, lastColumn, player) + consecutiveLeftUp(board, lastRow, lastColumn, player)
     console.log(verticalCheck, horizontalCheck, backslashCheck, forwardSlashCheck)
     if (verticalCheck || horizontalCheck >= 3 || backslashCheck >= 3 || forwardSlashCheck >=3){
-      this.setState({gameOver: true})
+      return true
+    } else{
+      return false
     }
   }
 
@@ -80,17 +78,21 @@ class App extends Component {
   }
 
   render(){
+    console.log(this.state.player, this.state.gameOver)
     return (
-      <div>
-      {this.state.board.map((column, value) => { 
-        return <Column 
-          key={value} 
-          number={value} 
-          pieces={column} 
-          setTurn={this.setTurn}
-        />
-      })}
-      </div>
+      <Container>
+        <Row>
+          {this.state.board.map((column, value) => { 
+            return <Column 
+              key={value} 
+              number={value} 
+              pieces={column} 
+              gameOver={this.state.gameOver}
+              addToColumn={this.addToColumn}
+            />
+          })}
+        </Row>
+      </Container>
     );
   }
 }
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Col} from 'reactstrap'
 
-const Column = ({ setTurn, number}) => {
-    return (<div onClick={() => setTurn(number)}>
-        {number}
-    </div>)
+const Column = ({ addToColumn, number, pieces }) => {
+    let fullColumn = [...pieces]
+    while (fullColumn.length < 6){
+        fullColumn.push(0)
+    }
+    return (<Col onClick={() => addToColumn(number)}>
+        {fullColumn.map((colNum) => colNum)}
+    </Col>)
 }
 
 export default Column;
