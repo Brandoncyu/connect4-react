03dda8398db842b2176f96a0f05476d0727a5cd7
Brandon <brandoncyu@gmail.com>
2019-05-15 13:24:02 -0700

    added new comments

--- src/App.js
+++ src/App.js
@@ -72,6 +72,11 @@ class App extends Component {
   }
 
   checkBoard = (board, lastRow, lastColumn, player) => {
+    //These functions check if the consecutive pieces horizontally, vertically, or diagnoally
+    //are equal to the new piece. The functions should return a value. If any of the new constance are 3
+    //or greater, then we have a connection of four consecutive pieces. The only exception is verticalCheck,
+    //which does not require combining a check in a different direction, so the logic is inheret to its 
+    //function. It should only return "true" or "false"
     const verticalCheck = consecutiveDown(board, lastRow, lastColumn, player)
     const horizontalCheck = consecutiveLeft(board, lastRow, lastColumn, player) + consecutiveRight(board, lastRow, lastColumn, player)
     const backslashCheck = consecutiveLeftDown(board, lastRow, lastColumn, player) + consecutiveRightUp(board, lastRow, lastColumn, player)
