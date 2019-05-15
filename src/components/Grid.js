49208936c811c4515c9af687e4c20998fbc16e93
Brandon <brandoncyu@gmail.com>
2019-05-15 13:44:56 -0700

    fixed read-me

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Row } from 'reactstrap'
 
 const Grid = ({ number }) => {
-    //This returns a colored grid. If the number passed in is "0", then it will 
-    //return a white grid. If it is "1", it will return a red grid. If it is "2",
-    //it will return a black grid.
+    //This returns a colored grid. If the number passed in is "0", then it will return a white grid. If it is "1", it will return a red grid. If it is "2",it will return a black grid.
     const color = ['white', 'red', 'black']
     const background = color[number]
     return (<Row>    
