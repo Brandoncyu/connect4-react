3f368e6d1d455914d22bd99024b4913f0418eb22
Brandon <brandoncyu@gmail.com>
2019-06-13 23:17:34 -0700

    added function

--- src/components/StartModal.js
+++ src/components/StartModal.js
@@ -6,7 +6,7 @@ const StartModal = ({ gameStart, toggleModal }) => {
         <ModalHeader toggle={() => toggleModal(false)}>Connect Four!</ModalHeader>
         <ModalBody>
             How Many Players Want to Connect Four?
-          </ModalBody>
+        </ModalBody>
         <ModalFooter>
             <Button color="primary" size="md" onClick={() => toggleModal(true)}>One Player</Button>{' '}
             <Button color="success" size="md" onClick={() => toggleModal(false)}>Two Players</Button>
